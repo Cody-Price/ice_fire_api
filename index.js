312119982129f2cdd8390e576275b00936f450bc
Taylor Moore <Tmoore2272@gmail.com>
2017-05-30 21:33:42 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const express = require('express');
+const cors = require('express-cors');
+const bodyParser = require('body-parser')
+const request = require('request');
+const port = process.env.PORT || 3001;
+const app = express();
+
+app.use(cors({
+    allowedOrigins: ['localhost:3000']
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/api/v1/houses', function(req, res) {
+  request('http://www.anapioficeandfire.com/api/houses?pageSize=50&hasTitles=true&hasSeats=true&hasAncestralWeapons=true',
+    function(error, response, body) {
+      if(error) {
+        console.log(error)
+        res.status(401).send(error)
+      }
+      res.status(200).send(body);
+    }
+  )
+});
+
+app.listen(port);
